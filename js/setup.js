9f319b07b50119d9bd141a5fdda7611669049ec9
Kotaina <kotaina217+git@gmail.com>
2020-06-27 10:19:39 +0300

    готовое

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,6 @@
 'use strict';
 var WIZARD_COUNT = 4;
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+var wizardsData = [];
 var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -15,23 +10,48 @@ var randomiser = function (rand) {
   return randomElement;
 };
 
-var getWizardData = [];
-
-for (var i = 0; i < WIZARD_COUNT; i++) {
-  var getUserObject = {
+var getWizardObject = function () {
+  var wizardObject = {
     name: WIZARD_NAMES[randomiser(WIZARD_NAMES.length)],
     surname: WIZARD_SURNAMES[randomiser(WIZARD_SURNAMES.length)],
     coatColor: COAT_COLORS[randomiser(COAT_COLORS.length)],
     eyesColor: EYES_COLOR[randomiser(EYES_COLOR.length)]
   };
+  return wizardObject;
+};
 
-  getWizardData.push(getUserObject);
+var generateWizardsData = function () {
+  for (var i = 0; i < WIZARD_COUNT; i++) {
+    wizardsData.push(getWizardObject);
+  }
+};
+
+var fillWizardTemplate = function (wizardObject) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = getWizardData[i].name + getWizardData[i].surname;
-  wizardElement.querySelector('.wizard-coat').style.fill = getWizardData[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = getWizardData[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizardObject.name + wizardObject.surname;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizardObject.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizardObject.eyesColor;
 
-  similarListElement.appendChild(wizardElement);
-}
+  return wizardElement;
+};
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+var renderWizards = function (wizards) {
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment;
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(fillWizardTemplate(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+
+var openPopup = function () {
+  document.querySelector('.setup').classList.remove('hidden');
+};
+
+generateWizardsData();
+renderWizards(wizardsData);
+openPopup();
